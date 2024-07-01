7151a8f4d9a1d0e812abeb8bb669f6b9978d81e5
azat <azat.amenov@gmail.com>
2024-07-01 22:38:13 +0500

    Weather UI 1

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,75 @@
 <template>
-  <div class="font-satoshi">
-    Medium
-    <Icon name="material-symbols:home-rounded" size="25px"></Icon>
+  <div class="background-page w-full h-screen flex font-satoshi font-normal">
+    <!-- Current weather background -->
+    <div class="current-weather h-full flex flex-col px-9 bg-white border-r">
+      <!-- Search -->
+<!--      <div class="search w-full relative">-->
+<!--        <input type="text" placeholder="Search City...." class="search-input w-full" />-->
+<!--        <span class="icon-wrapper absolute left-0 top-1/2 -translate-y-1/2 ml-9">-->
+<!--          <Icon name="ph:magnifying-glass" size="20px" />-->
+<!--        </span>
+      </div> -->
+      <div class="current-location">
+        <Icon name="ci:location" size="29px"/>
+        <span class="font-medium">Almaty</span>
+      </div>
+      <!-- Current weather -->
+      <div class="flex justify-center box-content gap-7 mt-5">
+        <img src="/assets/images/cloudy.png" alt="weather" class="current-weather-icon">
+        <div>
+          <span class="current-temp font-black">18°C</span>
+          <div class="temp-detail mb-2">
+            <img src="/assets/images/high-temp.png" alt="weather">
+            <span class="font-medium opacity-80">H: 25°C</span>
+          </div>
+          <div class="temp-detail">
+            <img src="/assets/images/low-temp.png" alt="weather">
+            <span class="font-medium opacity-80">L: 15°C</span>
+          </div>
+        </div>
+      </div>
+      <!-- Divider -->
+      <hr class="w-full mt-10 mb-6">
+      <!-- Current date -->
+      <div class="current-date">
+        <span class="font-medium opacity-55" style="font-size: 16.5px">Date Today</span>
+        <div class="flex gap-5">
+          <span class="font-black">Monday</span>
+          <span class="font-medium opacity-55">15:30</span>
+        </div>
+      </div>
+      <!-- Current weather details -->
+      <div class="weather-details w-full mt-7">
+        <span class="font-medium opacity-55" style="font-size: 16.5px">Weather Details</span>
+        <div class="detail-item mt-5">
+          <img src="/assets/images/cloud.png" alt="weather" style="width: 42px">
+          <span class="detail-item-span">Mostly Cloudly</span>
+        </div>
+        <div class="detail-item mt-3">
+          <img src="/assets/images/humidity.png" alt="weather">
+          <span class="detail-item-span">Humidity - 77%</span>
+        </div>
+        <div class="detail-item mt-3">
+          <img src="/assets/images/wind.png" alt="weather">
+          <span class="detail-item-span">Wind - 6km/h</span>
+        </div>
+      </div>
+    </div>
+    <!-- Right sight of the page -->
+    <div class="flex-1">
+      <header class="bg-white px-8 border-b flex items-center">
+        Header
+      </header>
+      <div> <!-- Other weather suggestions -->
+
+      </div>
+      <div> <!-- Weather suggestions in other cities -->
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
